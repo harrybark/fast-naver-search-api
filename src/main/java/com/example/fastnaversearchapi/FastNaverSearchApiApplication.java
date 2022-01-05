f7231865ec04558b1e7da3f77c0775d7843c3298
harrybark <harrypmw@gmail.com>
2022-01-05 22:56:35 +0900

    first commit

---
+++ src/main/java/com/example/fastnaversearchapi/FastNaverSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastnaversearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastNaverSearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastNaverSearchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fastnaversearchapi/FastNaverSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastnaversearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastNaverSearchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
